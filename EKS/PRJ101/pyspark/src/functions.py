ddd98bfdb78dc9974a35ba17eec73e11278af513
vknstudy96 <118576929+vknst96@users.noreply.github.com>
2022-12-13 20:58:07 +1100

    Add files via upload

--- EKS/PRJ101/pyspark/src/functions.py
+++ EKS/PRJ101/pyspark/src/functions.py
@@ -7,6 +7,61 @@ def loadWakeCountyCSV(spark, data_file):
             .option("header", "true")
             .load(data_file)
             )
+
+
+def loadDurhamCountyCSV(spark, data_file):
+    return (spark.read
+            .format("json")
+            .load(data_file)
+            )
+
+
+def transformWakeCountyDF(WakeCountyDF):
+    return (WakeCountyDF.withColumn("county", lit("Wake"))
+            .withColumnRenamed("HSISID", "datasetId")
+            .withColumnRenamed("NAME", "name")
+            .withColumnRenamed("ADDRESS1", "address1")
+            .withColumnRenamed("ADDRESS2", "address2")
+            .withColumnRenamed("CITY", "city")
+            .withColumnRenamed("STATE", "state")
+            .withColumnRenamed("POSTALCODE", "zip")
+            .withColumnRenamed("PHONENUMBER", "tel")
+            .withColumnRenamed("RESTAURANTOPENDATE", "dateStart")
+            .withColumn("dateEnd", lit(""))
+            .withColumnRenamed("FACILITYTYPE", "type")
+            .withColumnRenamed("X", "geoX")
+            .withColumnRenamed("Y", "geoY")
+            .drop("OBJECTID")
+            .drop("PERMITID")
+            .drop("GEOCODESTATUS")
+            .withColumn("id", concat("state", lit("_"), "county", lit("_"), "datasetId"))
+            )
+
+
+def transformDurhamCountyDF(DurhamCountyDF):
+    return (DurhamCountyDF.withColumn("county", lit("Durham"))
+            .withColumn("datasetId", col("fields.id"))
+            .withColumn("name", col("fields.premise_name"))
+            .withColumn("address1", col("fields.premise_address1"))
+            .withColumn("address2", col("fields.premise_address2"))
+            .withColumn("city", col("fields.premise_city"))
+            .withColumn("state", col("fields.premise_state"))
+            .withColumn("zip", col("fields.premise_zip"))
+            .withColumn("tel", col("fields.premise_phone"))
+            .withColumn("dateStart", col("fields.opening_date"))
+            .withColumn("dateEnd", col("fields.closing_date"))
+            .withColumn("type", split(col("fields.type_description"), "-").getItem(1))
+            .withColumn("geoX", col("fields.geolocation").getItem(0))
+            .withColumn("geoY", col("fields.geolocation").getItem(1))
+            .drop("fields")
+            .drop("geometry")
+            .drop("record_timestamp")
+            .drop("recordid")
+            .withColumn("id", concat("state", lit("_"), "county", lit("_"), "datasetId"))
+            )
+
+def combineDFs(df1,df2):
+        return df1.unionByName(df2)
         
 def print_df_summary(name, df):
     print(name, " df has ", df.count(), " rows")
--- EKS/PRJ101/pyspark/src/main.py
+++ EKS/PRJ101/pyspark/src/main.py
@@ -2,7 +2,7 @@ import sys
 
 from pyspark.sql import *
 
-from functions import loadWakeCountyCSV, print_df_summary
+from functions import loadWakeCountyCSV, transformWakeCountyDF, loadDurhamCountyCSV, transformDurhamCountyDF, combineDFs, print_df_summary
 
 
 if __name__ == "__main__":
@@ -12,8 +12,8 @@ if __name__ == "__main__":
         .appName("Rest")\
         .getOrCreate()
 
-    if len(sys.argv) != 2:
-        print("Usage: main <filename1>")
+    if len(sys.argv) != 3:
+        print("Usage: main <filename1> <filename2>")
         sys.exit(-1)
 
     print("Starting main")
@@ -26,6 +26,49 @@ if __name__ == "__main__":
 
     print("WakeCounty_raw_df is having ", WakeCounty_raw_df.count(), " records.")
 
-    print_df_summary('WakeCounty_raw_df',WakeCounty_raw_df)
+    WakeCounty_transformed_df = transformWakeCountyDF(WakeCounty_raw_df)
+    print("Right after wake county raw dataframe is transformed")
+    #WakeCounty_transformed_df.printSchema()
+    print(WakeCounty_transformed_df.schema.simpleString())
+
+    WakeCounty_transformed_df.show(5)
+    #print("WakeCounty_transformed_df has ", WakeCounty_transformed_df.rdd.getNumPartitions(), " Partitions")
+
+    #print(WakeCounty_transformed_df.schema)
+    #print(WakeCounty_transformed_df.schema.simpleString())
+    #print(WakeCounty_transformed_df.schema.json())
+
+    print(" ")
+    print("###########################################################################################################")
+    print("#2: Loading Json data file having data for restaurants in Durham county")
+    DurhamCounty_raw_df = loadDurhamCountyCSV(spark, sys.argv[2])
+    print("Right after raw data load of wake county dataset")
+    #DurhamCounty_raw_df.printSchema()
+    print(DurhamCounty_raw_df.schema.simpleString())
+    DurhamCounty_raw_df.show(5, truncate=False)
+
+    # The Json has nested data & arrays, which is bit difficult to handle
+    #to access the fields in a nested structure use dot(.) symbol in path.
+    #To access an element in an array, use the getItem() method
+    DurhamCounty_transformed_df = transformDurhamCountyDF(DurhamCounty_raw_df)
+
+    #DurhamCounty_transformed_df.printSchema()
+    print(DurhamCounty_transformed_df.schema.simpleString())
+    DurhamCounty_transformed_df.show(5, truncate=True)
+
+    print(" ")
+    print("###########################################################################################################")
+    print("#3: union the two dataframes")
+    union_df = combineDFs(WakeCounty_transformed_df,DurhamCounty_transformed_df)
+    union_df.show(5)
+    union_df.printSchema()
+
+    print_df_summary('WakeCounty_raw_df', WakeCounty_raw_df)
+    print_df_summary('WakeCounty_transformed_df', WakeCounty_transformed_df)
+
+    print_df_summary('DurhamCounty_raw_df', WakeCounty_raw_df)
+    print_df_summary('DurhamCounty_transformed_df', WakeCounty_transformed_df)
+
+    print_df_summary('union_df',union_df)
 
     spark.stop()
